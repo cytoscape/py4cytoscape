4ad79c525bbbaa71d9c39d9f40069623460a7886
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-02-12 15:17:09 -0800

    Added setup fields for keywords, package requirements, etc.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bdemchak", # Replace with your own username
+    name="bdemchak",
     version="0.0.1",
     author="Barry Demchak",
     author_email="bdemchak@ucsd.edu",
