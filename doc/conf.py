73541bb8af45f44077907cc9e07940bf5f9d62f3
Barry Demchak <bdemchak@ucsd.edu>
2020-02-13 11:45:54 -0800

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('./..'))
 # -- Project information -----------------------------------------------------
 
 project = 'PyCy3'
-copyright = '2020, Barry Demchak'
+copyright = '2020, The Cytoscape Consortium'
 author = 'Barry Demchak'
 
 # The short X.Y version
