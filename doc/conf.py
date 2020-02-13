f03eb5bbb8ac24f638639da6b1d02b31eea37892
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-02-12 17:23:36 -0800

    Experimenting with docstrings

--- doc/conf.py
+++ doc/conf.py
@@ -41,10 +41,11 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
 ]
-autosummary_generate = True
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::) in autodoc
+add_module_names = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
