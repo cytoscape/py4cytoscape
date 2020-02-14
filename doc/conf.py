d5d2463e22cebb73808fc08097de3405bfc7e28b
Barry Demchak <bdemchak@ucsd.edu>
2020-02-14 10:40:35 -0800

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, The Cytoscape Consortium'
 author = 'Barry Demchak'
 
 # The short X.Y version
-version = ''
+version = '0.0.1'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
@@ -83,7 +83,13 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# Alabaster is the default, but is too white, so I commented out: html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+def setup(app):
+  app.add_stylesheet( "css/tables.css" )
+# from http://stackoverflow.com/questions/32079200/how-do-i-set-up-custom-styles-for-restructuredtext-sphinx-readthedocs-etc/32079202#32079202
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
