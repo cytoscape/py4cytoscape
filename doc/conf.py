71841910e949d021ee5cb056cffb5aa65e88a37e
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-03-20 16:33:37 -0700

    Improving documentation layout

--- doc/conf.py
+++ doc/conf.py
@@ -95,18 +95,18 @@ html_favicon = '_static/images/cytoscape3-icon.ico'
 html_theme_options = {
 #    'canonical_url': '',
 #    'analytics_id': 'UA-XXXXXXX-1',  # Provided by Google in your dashboard
-    'logo_only': False,
-    'display_version': True,
+#    'logo_only': False,
+#    'display_version': True,
     'prev_next_buttons_location': 'top',
 #    'style_external_links': False,
 #    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'blue', # This controls the background for the logo in the upper left corner
+#    'style_nav_header_background': 'blue', # This controls the background for the logo in the upper left corner
     # Toc options
-    'collapse_navigation': True,
+#    'collapse_navigation': True,
 #    'sticky_navigation': True,
 #    'navigation_depth': 4,
 #    'includehidden': True,
-    'titles_only': True
+#    'titles_only': True
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
