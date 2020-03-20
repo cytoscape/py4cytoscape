3603b92388357225d4b56045ee82c9017724a67c
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-03-20 16:28:09 -0700

    Improving documentation layout

--- doc/conf.py
+++ doc/conf.py
@@ -100,13 +100,13 @@ html_theme_options = {
 #    'prev_next_buttons_location': 'bottom',
 #    'style_external_links': False,
 #    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white', # This controls the background for the logo in the upper left corner
+    'style_nav_header_background': 'black', # This controls the background for the logo in the upper left corner
     # Toc options
-    'collapse_navigation': True,
+    'collapse_navigation': False,
 #    'sticky_navigation': True,
 #    'navigation_depth': 4,
 #    'includehidden': True,
-#    'titles_only': False
+    'titles_only': True
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
