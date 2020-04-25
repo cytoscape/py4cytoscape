d6b968fdca5d82bd8a7c69a49d505a8e0af42690
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-04-24 18:42:39 -0700

    Made logger show nesting and break between highest level API calls

--- PyCy3/pycy3_logger.py
+++ PyCy3/pycy3_logger.py
@@ -84,27 +84,41 @@ _logger_debug = 'DEBUG' in [_FILE_LOG_LEVEL, _CONSOLE_LOG_LEVEL]
 
 
 # Decorator so functions can get automatic logging
-
-# TODO: Consider indenting entries to indicate nesting
+_logger_nesting = -1
 def cy_log(func):
     """Log function call parameters and results"""
 
     @functools.wraps(func)
     def wrapper_log(*args, **kwargs):
+        # Set up to show logged calls within logged calls so the appear nested in the log
+        global _logger_nesting
+        _logger_nesting += 1
+        nesting_spacer = '\u01c0' * _logger_nesting # Use latin dental click character to represent spacing = nesting
+
         if _logger_debug:
+            # Show function name and all positional and named arguments
             args_repr = [repr(a) for a in args]
             kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
             signature = ", ".join(args_repr + kwargs_repr)
-            logger.debug(f"Calling {func.__name__}({signature})")
-        logger.info(f"Into {func.__name__}()")
+            logger.debug(f"{nesting_spacer}Calling {func.__name__}({signature})")
+
+        logger.info(f"{nesting_spacer}Into {func.__name__}()")
         try:
-            value = func(*args, **kwargs)
-            logger.info(f"Out of {func.__name__!r}")
-            if _logger_debug: logger.debug(f"Returning {func.__name__!r}: {value!r}")
+            value = func(*args, **kwargs) # Call function being logged
+
+            # Show function return value
+            logger.info(f"{nesting_spacer}Out of {func.__name__!r}")
+            if _logger_debug: logger.debug(f"{nesting_spacer}Returning {func.__name__!r}: {value!r}")
             return value
         except Exception as e:
-            logger.info(f"Exception from {func.__name__!r}")
-            if _logger_debug: logger.debug(f"{func.__name__!r} exception {e!r}")
+            # Show function exception
+            logger.info(f"{nesting_spacer}Exception from {func.__name__!r}")
+            if _logger_debug: logger.debug(f"{nesting_spacer}{func.__name__!r} exception {e!r}")
             raise
+        finally:
+            _logger_nesting -= 1
+            if _logger_nesting == -1:
+                if _logger_debug: logger.debug('-' * 20)
+                logger.info('-' * 20)
 
     return wrapper_log
