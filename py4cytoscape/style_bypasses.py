d3107c6f424c64214e461217a05f004931766602
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-05-24 13:13:48 -0700

    Reorganized document sections

--- py4cytoscape/style_bypasses.py
+++ py4cytoscape/style_bypasses.py
@@ -8,22 +8,6 @@ II. Specific functions for setting particular node, edge and network properties
 NOTE: The CyREST 'bypass' endpoint is essential to properly set values that
 will persist for a given network independent of applied style and style
 changes, and from session to session if saved.
-
-License:
-    Copyright 2020 The Cytoscape Consortium
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-    documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-    and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions
-    of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-    WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-    OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 # External library imports
