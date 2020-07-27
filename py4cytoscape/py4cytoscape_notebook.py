85d6b548c4d61846fd56ce04ee4f66cc88774558
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-07-27 15:07:06 -0700

    Updated for SSL

--- py4cytoscape/py4cytoscape_notebook.py
+++ py4cytoscape/py4cytoscape_notebook.py
@@ -56,7 +56,7 @@ class SpoofResponse:
 def do_request_remote(method, url, **kwargs):
 #        JUPYTER_BRIDGE_URL = 'http://127.0.0.1:9529' # For local testing
 #        JUPYTER_BRIDGE_URL = 'http://192.168.2.194:9529' # For production
-    JUPYTER_BRIDGE_URL = 'http://70.95.64.191:9529' # For production
+    JUPYTER_BRIDGE_URL = 'https://70.95.64.191:9529' # For production
 
     log_http_request(method, url, **kwargs)
 
