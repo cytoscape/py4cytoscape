da8c147615f2d95a337b0d0dcf863503b5fe8f13
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-08-08 14:16:22 -0700

    Tested direct loading of JS

--- py4cytoscape/py4cytoscape_notebook.py
+++ py4cytoscape/py4cytoscape_notebook.py
@@ -171,7 +171,8 @@ def check_running_remote():
             except:
                 # Local Cytoscape doesn't appear to be reachable, so try reaching a remote Cytoscape via Jupyter-bridge
                 try:
-#                    detail_logger.debug('JS: ' + os.path.join(os.path.dirname(__file__), 'howdy.js'))
+                    detail_logger.debug('JS: ' + os.path.join(os.path.dirname(__file__), 'howdy.js'))
+                    detail_logger.debug('py4: ' + os.getcwd())
                     do_request_remote('GET', 'http://localhost:1234/v1', headers={'Content-Type': 'application/json'})
                     _running_remote = True
 
