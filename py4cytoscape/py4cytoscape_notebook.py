2382b858f96904b10e2765e38da6be3f39163c79
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-08-20 14:49:00 -0700

    Use only 127.0.0.1 instead of localhost (to avoid hijack by Colab)

--- py4cytoscape/py4cytoscape_notebook.py
+++ py4cytoscape/py4cytoscape_notebook.py
@@ -201,7 +201,7 @@ def get_browser_client_js():
             'https://raw.githubusercontent.com/bdemchak/jupyter-bridge/master/client/javascript_bridge.js')
         r.raise_for_status()
         return f'var Channel = "{_CHANNEL}"; \n\n' \
-               f'var JupyterBridge = {_JUPYTER_BRIDGE_URL}; \n\n' \
+               f'var JupyterBridge = "{_JUPYTER_BRIDGE_URL}"; \n\n' \
                f' {r.text}'
     except Exception as e:
         raise requests.exceptions.HTTPError(f'Error creating Jupyter-bridge browser client for channel {_CHANNEL}: {_error_content(e)}')
