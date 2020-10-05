ec0c2463e87f13cd8a52d03e6dac85219d75eeed
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-10-05 15:14:42 -0700

    Change Twine to upload a specific version

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 rem ... from https://packaging.python.org/tutorials/packaging-projects/
+rem ... remember to update twine command (below) to contain new version
 rem ... remember to bump the version number in setup.py before uploading
 
 python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
+python3 -m twine upload dist/py4cytoscape-0.0.3*
 
 
