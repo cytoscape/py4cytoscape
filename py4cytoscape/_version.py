c57beab5ada83f02037c7e34687f1152af35a145
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-10-06 14:37:42 -0700

    Testing notebook test

--- py4cytoscape/_version.py
+++ py4cytoscape/_version.py
@@ -22,7 +22,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 # This is the official version of this library. Change it here, and the build and documentation
 # should pick it up automatically.
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 # This reflects the reference API this module meets
 _automation_api_version = '1.0.0'
