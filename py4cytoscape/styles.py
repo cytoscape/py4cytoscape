5f80250702b9629698870345a46bc07b7c58af96
Yihang Xin <yxin@LAXUCR08173.local>
2021-01-12 21:14:48 -0800

    Fixed issue #22

--- py4cytoscape/styles.py
+++ py4cytoscape/styles.py
@@ -9,17 +9,17 @@ II. General property functions
 
 """Copyright 2020 The Cytoscape Consortium
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
 persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
 Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
-THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
@@ -31,6 +31,7 @@ import warnings
 from . import commands
 from . import networks
 from . import sandbox
+from . import network_views
 
 # Internal module convenience imports
 from .exceptions import CyError
@@ -390,3 +391,30 @@ def get_visual_property_names(base_url=DEFAULT_BASE_URL):
     res = commands.cyrest_get('styles/default/defaults', base_url=base_url)
     visual_properties = [prop['visualProperty']     for prop in res['defaults']]
     return visual_properties
+
+
+@cy_log
+def get_current_style(network=None, base_url=DEFAULT_BASE_URL):
+    """Get the current visual style applied to a network.
+
+    Args:
+        network (str or SUID or None): Name or SUID of the network or view. Default is the "current" network active in Cytoscape.
+                If a network view SUID is provided, then it is validated and returned.
+        base_url (str): Ignore unless you need to specify a custom domain,
+            port or version to connect to the CyREST API. Default is http://127.0.0.1:1234
+            and the latest version of the CyREST API supported by this version of py4cytoscape.
+
+    Returns:
+        str: Name of style
+
+    Raises:
+        requests.exceptions.RequestException: if can't connect to Cytoscape or Cytoscape returns an error
+
+    Examples:
+        >>> get_current_style()
+        "dafault"
+    """
+    net_suid = networks.get_network_suid(network, base_url=base_url)
+    view_suid = network_views.get_network_view_suid(net_suid, base_url=base_url)
+    res = commands.cyrest_get(f'networks/{net_suid}/views/{view_suid}/currentStyle', base_url=base_url)
+    return res['title']
