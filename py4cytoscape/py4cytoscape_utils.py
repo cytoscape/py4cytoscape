f4ae3490cb5ab3915a7ae59937985ac4073aea70
Barry Demchak <idekerlab.bdemchak@gmail.com>
2022-05-13 19:01:33 -0400

    Fixed node_name_to_node_suid to accept node names that look like numbers (e.g., Entrez-Gene)

--- py4cytoscape/py4cytoscape_utils.py
+++ py4cytoscape/py4cytoscape_utils.py
@@ -721,7 +721,8 @@ def _item_to_suid(item_names, table_name, network=None, base_url=DEFAULT_BASE_UR
     except:
         pass
 
-    # map all names into SUIDs ... all names *must* be actual names
+    # map all names into SUIDs ... all names *must* be actual names ... and must be str() to match the 'name' column
+    item_names = [str(item_name)   for item_name in item_names]
     item_name_to_suid_list = {item_name: list(df[df.name.eq(item_name)].index.values) for item_name in item_names}
     try:
         if unique_list:
