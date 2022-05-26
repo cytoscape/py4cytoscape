c0f6c13e00928c05485fa2bcc35d292418d3738a
Barry Demchak <idekerlab.bdemchak@gmail.com>
2022-05-26 17:13:23 -0400

    Removed --user parameter to avoid error "Will not install to the user site because it will lack sys.path precedence to setuptools "

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ rem ... from https://packaging.python.org/tutorials/packaging-projects/
 rem ... remember to update twine command (below) to contain new version
 rem ... remember to bump the version number in _version.py before uploading
 
-python -m pip install --user --upgrade setuptools wheel twine
+python -m pip install --upgrade setuptools wheel twine
 python setup.py sdist bdist_wheel
 python -m twine upload dist/py4cytoscape-1.3.0*
 
